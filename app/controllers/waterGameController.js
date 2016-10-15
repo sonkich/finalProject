d9c01e4c56b462de013ba7e3ad03e2df305d86fa
Antonio Dyulgerski <adyulgerski94@gmail.com>
2016-10-15 23:44:05 +0300

    water game

--- app/controllers/waterGameController.js
+++ app/controllers/waterGameController.js
@@ -1,90 +1,382 @@
-myApp.controller("waterGameController",function($scope){
+myApp.controller("waterGameController",function($scope,$rootScope,homeSvc){
+   $scope.points = 0;
+   $scope.bullets = 10;
+   $scope.waterList = shuffle(waterList);
+   $scope.diamonds = 0;
+   $scope.water = 0;
 
+   $scope.reset = function(){
+      for(water in waterList){
+         waterList[water].visible = false;
+      }
+
+      $scope.points = 0;
+      $scope.bullets = 10;
+      $scope.waterList = shuffle(waterList);
+      $scope.diamonds = 0;
+      $scope.water = 0;
+   }
+
+   $scope.turnVisible = function(index){
+      if($scope.bullets > 1){
+
+         $scope.bullets -= 1;
+         $scope.points += $scope.waterList[index].points;
+         $scope.waterList[index].visible = true;
+      }else if($scope.bullets == 1){
+         $scope.bullets -= 1;
+         $scope.points += $scope.waterList[index].points;
+         $scope.waterList[index].visible = true;
+
+         calcProfit();
+
+         // end game give points here
+      }
+   }
+
+   function calcProfit (){
+
+      if($scope.points < 50){
+         $scope.water = 1;
+      }else if($scope.points >= 50 && $scope.points < 100){
+         $scope.water = 2;
+         $scope.diamonds = 1;
+      }else if($scope.points >= 100 && $scope.points < 150){
+         $scope.water = 3;
+         $scope.diamonds = 2;
+      }else {
+         $scope.water = 5;
+         $scope.diamonds = 5;
+      }
+
+      $rootScope.player.drink_q = parseInt($rootScope.player.drink_q) + $scope.water;
+      $rootScope.player.diamonds = parseInt($rootScope.player.diamonds) + $scope.diamonds;
+      homeSvc.setData($rootScope.baby, $rootScope.player);
+   }
 });
 
-var iconsList = [
+function shuffle(array) {
+    var counter = array.length;
 
-{
-   name : "duck",
-   url : "./assets/img/game1/duck.png",
-   visible : false
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        var index = Math.floor(Math.random() * counter);
 
-},
-{
-   name : "chameleon",
-   url : "./assets/img/game1/chameleon.png",
-   visible : false
-},
-{
-   name : "lion",
-   url : "./assets/img/game1/lion.png",
-   visible : false
-},
-{
-   name : "monkey",
-   url : "./assets/img/game1/monkey.png",
-   visible : false
-},
-{
-   name : "owl",
-   url : "./assets/img/game1/owl.png",
-   visible : false
-},
-{
-   name : "penguin",
-   url : "./assets/img/game1/penguin.png",
-   visible : false
-},
-{
-   name : "sloth",
-   url : "./assets/img/game1/sloth.png",
-   visible : false
-},
-{
-   name : "squirrel",
-   url : "./assets/img/game1/squirrel.png",
-   visible : false
-},
-{
-   name : "duck",
-   url : "./assets/img/game1/duck.png",
-   visible : false
+        // Decrease counter by 1
+        counter--;
 
-},
-{
-   name : "chameleon",
-   url : "./assets/img/game1/chameleon.png",
-   visible : false
-},
-{
-   name : "lion",
-   url : "./assets/img/game1/lion.png",
-   visible : false
-},
-{
-   name : "monkey",
-   url : "./assets/img/game1/monkey.png",
-   visible : false
-},
-{
-   name : "owl",
-   url : "./assets/img/game1/owl.png",
-   visible : false
-},
-{
-   name : "penguin",
-   url : "./assets/img/game1/penguin.png",
-   visible : false
-},
-{
-   name : "sloth",
-   url : "./assets/img/game1/sloth.png",
-   visible : false
-},
-{
-   name : "squirrel",
-   url : "./assets/img/game1/squirrel.png",
-   visible : false
+        // And swap the last element with it
+        var temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
 }
+
+var waterList = [
+
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "drop",
+      url : "./assets/img/game2/drop.png",
+      visible : false ,
+      points : 1
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "water",
+      url : "./assets/img/game2/water.png",
+      visible : false ,
+      points : 10
+   },
+   {
+      name : "max-water",
+      url : "./assets/img/game2/max-water.png",
+      visible : false ,
+      points : 50
+   },
+   {
+      name : "max-water",
+      url : "./assets/img/game2/max-water.png",
+      visible : false ,
+      points : 50
+   }
+
 
 ];
