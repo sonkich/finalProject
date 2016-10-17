8ef84f433883ccfb04bfd7851ed73e92576af998
sisi-mileva <sisi_ivko1@yahoo.com>
2016-10-17 03:00:06 +0300

    home change/friend-dependence

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,9 +1,11 @@
-myApp.controller("homeController",function($scope, $rootScope,  $location, $route, $routeParams, homeSvc, loggedUserSvc, $timeout){
+myApp.controller("homeController",function($scope, $rootScope,  $location, $route, $routeParams, 
+		$http, $httpParamSerializerJQLike, homeSvc, loggedUserSvc, $timeout){
 	
 	$scope.error = '';
 	$scope.babyName = '';
 	$rootScope.baby = {};
 	$rootScope.player = {};
+	$scope.friends;
 	$scope.playing = false;
 	$scope.sound = './assets/img/sound_on.png';
 	$scope.bottle = 0;
@@ -38,10 +40,14 @@ myApp.controller("homeController",function($scope, $rootScope,  $location, $rout
 					homeSvc.getBaby(parent).then(function(result) {
 						$rootScope.baby = result;
 						console.log(result);
-						if ($rootScope.baby.gender == 'm') {
-							$scope.babyImage = './assets/img/boy1.png'
+						if (loggedUserSvc.getInfo().is_alive == 0) {
+							$scope.babyImage = './assets/img/gost.png';
 						} else {
-							$scope.babyImage = './assets/img/girl1.png'
+							if ($rootScope.baby.gender == 'm') {
+								$scope.babyImage = './assets/img/boy1.png';
+							} else {
+								$scope.babyImage = './assets/img/girl1.png';
+							}
 						}
 					})
 				})
@@ -50,6 +56,7 @@ myApp.controller("homeController",function($scope, $rootScope,  $location, $rout
 		}
 	}
 	loadGame();
+	updateFriends();
 	
 	$scope.createBaby = function(item) {
 		if ($scope.babyName == '') {
@@ -98,69 +105,75 @@ myApp.controller("homeController",function($scope, $rootScope,  $location, $rout
 	}
 	
 	$scope.play = function(item) {
-		if ($scope.playing == false) {
-			$scope.playing = true;
-			if (item.currentTarget.getAttribute("id") == 'bottle') {
-				if ($rootScope.player.drink_q > 0){
-					if ($rootScope.baby.drink < 100) {
-						playAudio('./assets/sounds/eat.mp3');
-						if (parseInt($rootScope.baby.drink) + 5 > 100) {
-							$rootScope.baby.drink = 100;
-						} else {
-							$rootScope.baby.drink = parseInt($rootScope.baby.drink) + 5;
-						}
-						$rootScope.player.drink_q -= 1;
-						
-						$rootScope.player.points = parseInt($rootScope.player.points) + 1;
-						homeSvc.setData($rootScope.baby, $rootScope.player);
-						if ($rootScope.baby.gender == 'm') {
-							imageChange('./assets/img/boy_drink.png', './assets/img/boy1.png', 3000); 
-						} else {
-							imageChange('./assets/img/girl_drink.png', './assets/img/girl1.png', 3000);
-							$scope.bottle = 1;
-							$timeout(function() {
-								$scope.bottle = 0;
-							}, 3000);
-						}
-					}
-				}
-			} else if(item.currentTarget.getAttribute("id") == 'jar') {
-				if ($rootScope.player.food_q > 0){
-					if ($rootScope.baby.food < 100) {
-						playAudio('./assets/sounds/eat.mp3');
-						if (parseInt($rootScope.baby.food) + 1 + parseInt($rootScope.player.food_lvl) > 100) {
-							$rootScope.baby.food = 100;
-						} else {
-							$rootScope.baby.food = parseInt($rootScope.baby.food) + 1 + parseInt($rootScope.player.food_lvl);
-						}
-						$rootScope.player.food_q -= 1;
-						
-						$rootScope.player.points = parseInt($rootScope.player.points) + 1 + parseInt($rootScope.player.food_lvl);
-						homeSvc.setData($rootScope.baby, $rootScope.player);
-						if ($rootScope.baby.gender == 'm') {
-							imageChange('./assets/img/boy_eat.png', './assets/img/boy1.png', 3000); 
-						} else {
-							imageChange('./assets/img/girl_eat.png', './assets/img/girl1.png', 3000) 
-						}
-					}
-				}
+		if ($routeParams.user == localStorage.getItem("username") || isFriend()) {
+			if (loggedUserSvc.getInfo().is_alive == 0) {
+				$scope.show = 1;
 			} else {
-				if ($rootScope.player.toys_q > 0){
-					if ($rootScope.baby.happiness < 100) {
-						playAudio('./assets/sounds/laugh.mp3');
-						if (parseInt($rootScope.baby.happiness) + 1 + parseInt($rootScope.player.cloth_lvl) > 100) {
-							$rootScope.baby.happiness = 100;
-						} else {
-							$rootScope.baby.happiness = parseInt($rootScope.baby.happiness) + 1 + parseInt($rootScope.player.cloth_lvl);
+				if ($scope.playing == false) {
+					$scope.playing = true;
+					if (item.currentTarget.getAttribute("id") == 'bottle') {
+						if ($rootScope.player.drink_q > 0){
+							if ($rootScope.baby.drink < 100) {
+								playAudio('./assets/sounds/eat.mp3');
+								if (parseInt($rootScope.baby.drink) + 5 > 100) {
+									$rootScope.baby.drink = 100;
+								} else {
+									$rootScope.baby.drink = parseInt($rootScope.baby.drink) + 5;
+								}
+								$rootScope.player.drink_q -= 1;
+								
+								$rootScope.player.points = parseInt($rootScope.player.points) + 1;
+								homeSvc.setData($rootScope.baby, $rootScope.player);
+								if ($rootScope.baby.gender == 'm') {
+									imageChange('./assets/img/boy_drink.png', './assets/img/boy1.png', 3000); 
+								} else {
+									imageChange('./assets/img/girl_drink.png', './assets/img/girl1.png', 3000);
+									$scope.bottle = 1;
+									$timeout(function() {
+										$scope.bottle = 0;
+									}, 3000);
+								}
+							}
 						}
-						$rootScope.player.toys_q -= 1;
-						
-						$rootScope.player.points = parseInt($rootScope.player.points) + 1 + parseInt($rootScope.player.cloth_lvl);
-						homeSvc.setData($rootScope.baby, $rootScope.player);
-						if ($rootScope.baby.gender == 'm') {
-							imageChange('./assets/img/boy_play.png', './assets/img/boy1.png', 3000); 
-						} else {
-							imageChange('./assets/img/girl_play.png', './assets/img/girl1.png', 3000) 
+					} else if(item.currentTarget.getAttribute("id") == 'jar') {
+						if ($rootScope.player.food_q > 0){
+							if ($rootScope.baby.food < 100) {
+								playAudio('./assets/sounds/eat.mp3');
+								if (parseInt($rootScope.baby.food) + 1 + parseInt($rootScope.player.food_lvl) > 100) {
+									$rootScope.baby.food = 100;
+								} else {
+									$rootScope.baby.food = parseInt($rootScope.baby.food) + 1 + parseInt($rootScope.player.food_lvl);
+								}
+								$rootScope.player.food_q -= 1;
+								
+								$rootScope.player.points = parseInt($rootScope.player.points) + 1 + parseInt($rootScope.player.food_lvl);
+								homeSvc.setData($rootScope.baby, $rootScope.player);
+								if ($rootScope.baby.gender == 'm') {
+									imageChange('./assets/img/boy_eat.png', './assets/img/boy1.png', 3000); 
+								} else {
+									imageChange('./assets/img/girl_eat.png', './assets/img/girl1.png', 3000) 
+								}
+							}
+						}
+					} else {
+						if ($rootScope.player.toys_q > 0){
+							if ($rootScope.baby.happiness < 100) {
+								playAudio('./assets/sounds/laugh.mp3');
+								if (parseInt($rootScope.baby.happiness) + 1 + parseInt($rootScope.player.cloth_lvl) > 100) {
+									$rootScope.baby.happiness = 100;
+								} else {
+									$rootScope.baby.happiness = parseInt($rootScope.baby.happiness) + 1 + parseInt($rootScope.player.cloth_lvl);
+								}
+								$rootScope.player.toys_q -= 1;
+								
+								$rootScope.player.points = parseInt($rootScope.player.points) + 1 + parseInt($rootScope.player.cloth_lvl);
+								homeSvc.setData($rootScope.baby, $rootScope.player);
+								if ($rootScope.baby.gender == 'm') {
+									imageChange('./assets/img/boy_play.png', './assets/img/boy1.png', 3000); 
+								} else {
+									imageChange('./assets/img/girl_play.png', './assets/img/girl1.png', 3000) 
+								}
+							}
 						}
 					}
 				}
@@ -169,7 +182,9 @@ myApp.controller("homeController",function($scope, $rootScope,  $location, $rout
 	}
 	
 	$scope.openStore = function() {
-		$location.path('/store');
+		if ($routeParams.user == localStorage.getItem("username")) {
+			$location.path('/store');
+		}
 	}
 	
 	$scope.sleep = function() {
@@ -205,5 +220,31 @@ myApp.controller("homeController",function($scope, $rootScope,  $location, $rout
 	function playAudio(u) {
 		audio.src = u;
 		audio.play();
+	}
+	
+	function updateFriends(){
+	      var data = {
+	         "username" : loggedUserSvc.getInfo().username
+	      }
+	      $http({
+	           method: 'POST',
+	           url: './server/getFriends.php',
+	           headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+	           data : $httpParamSerializerJQLike(data)
+	         }).then(function successCallback(response) {
+	            $scope.friends = response.data;
+	            console.log($scope.friends);
+	           }, function errorCallback(response) {
+	            console.log("ERROR");
+	         });
+	}
+	
+	function isFriend() {
+		for (var i = 0; i < $scope.friends.length; i++) {
+			if ($routeParams.user == $scope.friends[i].username2) {
+				return true;
+			}
+		}
+		return false;
 	}
 });
