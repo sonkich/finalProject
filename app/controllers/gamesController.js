56cb2155f4adb9ee5de2917497bb082584b48354
Antonio Dyulgerski <adyulgerski94@gmail.com>
2016-10-18 16:48:32 +0300

    profile upd and games buttons

--- app/controllers/gamesController.js
+++ app/controllers/gamesController.js
@@ -1,3 +1,11 @@
-myApp.controller("gamesController",function($scope){
-
+myApp.controller("gamesController",function($scope,$location){
+   $scope.playForFood = function(){
+      $location.path('/games/foodGame');
+   }
+   $scope.playForWater = function(){
+      $location.path('/games/waterGame');
+   }
+   $scope.playForToys = function(){
+      $location.path('/games/toyGame');
+   }
 });
